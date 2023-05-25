01e3740b4f25e0352dd1b58bc469e29c5d3cf85d
dri <dri@novatec-gmbh.de>
2023-05-25 13:24:17 +0200

    Add first contract

---
+++ producer/src/test/resources/contracts/Orders.groovy
@@ -0,0 +1,21 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "should return an order"
+    name "get order"
+    request{
+        method GET()
+        url("/order") {
+            queryParameters {
+                parameter("number", value(consumer(matching("[0-9]+")), producer(3)))
+            }
+        }
+    }
+    response {
+        body(
+                id:fromRequest().query("number"),
+                name:anyNonEmptyString(),
+        )
+        status 200
+    }
+}
